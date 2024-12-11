1152d0661110ecca6237b1e724e47d628f7d6c8b
JeffersonCallupe <jeff.callupe@gmail.com>
2024-12-11 00:34:47 -0500

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import ActividadesRegistradas from "./app/dashboard/ActividadesRegistradas";
 import ActividadesHistorico from "./app/dashboard/ActividadesHistorico";
 import Actividades from "./app/dashboard/Actividades";
 import Configuraciones from "./app/profile/Configuraciones";
-import Home from "./app/dashboard/home";
+import Home from "./app/dashboard/Home";
 import LoginInstitucional from "../src/app/login/LoginInstitucional";
 import LoginEmpresa from '../src/app/login/LoginEmpresa';
 import LoginSUM from "../src/app/login/LoginSUM";
-import OfertasLaborales from "./app/dashboard/ofertasLaborales";
+import OfertasLaborales from "./app/dashboard/OfertasLaborales";
 import OfertasAplicadas from "./app/dashboard/OfertasAplicadas";
 import OfertasHistorico from "./app/dashboard/OfertasHistorico";
 import ProfileEmpresa from "./app/profile/ProfileEmpresa"
 import ProfileInstitucional from "./app/profile/ProfileInstitucional";
-import RegistroSUM from "./app/registro/registroSUM";
-import RegistroEmpresa from "./app/registro/registroEmpresa";
+import RegistroSUM from "./app/registro/RegistroSUM";
+import RegistroEmpresa from "./app/registro/RegistroEmpresa";
 import { UserProvider } from "./contexts/userContext";
 import { AlertProvider } from "./contexts/alertContext";
 
@@ -32,13 +32,13 @@ function App() {
             <Route path="/profile" element={<ProfileEmpresa />} />
             <Route path="/profileInstitucional" element={<ProfileInstitucional />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/actividades" element={<Actividades />} />
-            <Route path="/actividadesRegistradas" element={<ActividadesRegistradas />} />
-            <Route path="/actividadesHistorico" element={<ActividadesHistorico />} />
-            <Route path="/ofertasLaborales" element={<OfertasLaborales />} />
-            <Route path="/ofertasAplicadas" element={<OfertasAplicadas />} />
-            <Route path="/ofertasHistorico" element={<OfertasHistorico />} />
-            <Route path="/settings" element={<Configuraciones />} />
+            <Route path="/Actividades" element={<Actividades />} />
+            <Route path="/ActividadesRegistradas" element={<ActividadesRegistradas />} />
+            <Route path="/ActividadesHistorico" element={<ActividadesHistorico />} />
+            <Route path="/AfertasLaborales" element={<OfertasLaborales />} />
+            <Route path="/OfertasAplicadas" element={<OfertasAplicadas />} />
+            <Route path="/OfertasHistorico" element={<OfertasHistorico />} />
+            <Route path="/Configuraciones" element={<Configuraciones />} />
           </Routes>
         </BrowserRouter>
       </UserProvider>
